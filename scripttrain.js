fc664a4848d3e1074d67ed9a06f5c0686a3ce4b1
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-10 13:41:16 +0700

    add Stype,Bo,Num type

---
+++ scripttrain.js
@@ -0,0 +1,25 @@
+//String type
+let firstname = 'F';
+let lastname = 'R';
+
+let Fullname = firstname + ' ' + lastname;
+console.log(Fullname);
+
+//Boolean type
+let email = 'test@gmail.com';
+let findletter = email.includes('e');//includes = ตรวจเช็ค
+console.log(findletter);
+
+//Number type
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius,pi);
+
+console.log(10/2);
+let result = radius % 3;
+console.log(result);
+
+//the order of the operations is important - B I D M A S
+let bidMas = 5 * (10-3)**2;
+console.log(bidMas);
