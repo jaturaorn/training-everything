2f3085803e856560266ac75cef2d5ce8eed4158e
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-10 14:17:32 +0700

    add OBJ, kv and method

--- scripttrain.js
+++ scripttrain.js
@@ -5,3 +5,19 @@ selectedFruits [4] = 5;//add
 selectedFruits [1] = 'pear';//add
 console.log(selectedFruits[1]); //acess
 console.log(selectedFruits.length); //length
+
+//OBJECT type (key, value)
+let user = { //this obj
+    firstname: 'F',//key:,value
+    lastname: 'G',
+    email: 'test@test.com',
+    age: 26,
+    location: 'bangkok',
+    readlist:['html','css','javascript']
+}
+
+console.log(user);
+console.log(user.email);
+user.age = 60;
+console.log(user.age);
+console.log(user.readlist.length-1); //length
