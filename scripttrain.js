e9aa1b962ca7474767d73701a9556b95cce7a0f1
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-10 14:32:32 +0700

    changing data type,link to html

--- scripttrain.js
+++ scripttrain.js
@@ -1,23 +1,29 @@
-//Array type
-let selectedFruits = ['apple', 'banana', 'orange'];
-selectedFruits [3]  = 'pineapple';//add
-selectedFruits [4] = 5;//add
-selectedFruits [1] = 'pear';//add
-console.log(selectedFruits[1]); //acess
-console.log(selectedFruits.length); //length
+// //Array type
+// let selectedFruits = ['apple', 'banana', 'orange'];
+// selectedFruits [3]  = 'pineapple';//add
+// selectedFruits [4] = 5;//add
+// selectedFruits [1] = 'pear';//add
+// console.log(selectedFruits[1]); //acess
+// console.log(selectedFruits.length); //length
 
-//OBJECT type (key, value)
-let user = { //this obj
-    firstname: 'F',//key:,value
-    lastname: 'G',
-    email: 'test@test.com',
-    age: 26,
-    location: 'bangkok',
-    readlist:['html','css','javascript']
-}
+// //OBJECT type (key, value)
+// let user = { //this obj
+//     firstname: 'F',//key:,value
+//     lastname: 'G',
+//     email: 'test@test.com',
+//     age: 26,
+//     location: 'bangkok',
+//     readlist:['html','css','javascript']
+// }
 
-console.log(user);
-console.log(user.email);
-user.age = 60;
-console.log(user.age);
-console.log(user.readlist.length-1); //length
+// console.log(user);
+// console.log(user.email);
+// user.age = 60;
+// console.log(user.age);
+// console.log(user.readlist.length-1); //length
+
+//chaging data type
+let x = 123;
+x = 'F';
+console.log(x);
+console.log(typeof x); //type of
