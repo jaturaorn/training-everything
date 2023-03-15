f7f90cc3d8429f16029755c47ff4c01bcb75680f
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-15 13:37:29 +0700

    test method

--- scripttrain.js
+++ scripttrain.js
@@ -1,29 +1,48 @@
-// //Array type
-// let selectedFruits = ['apple', 'banana', 'orange'];
-// selectedFruits [3]  = 'pineapple';//add
-// selectedFruits [4] = 5;//add
-// selectedFruits [1] = 'pear';//add
-// console.log(selectedFruits[1]); //acess
-// console.log(selectedFruits.length); //length
+// // //Array type
+// // let selectedFruits = ['apple', 'banana', 'orange'];
+// // selectedFruits [3]  = 'pineapple';//add
+// // selectedFruits [4] = 5;//add
+// // selectedFruits [1] = 'pear';//add
+// // console.log(selectedFruits[1]); //acess
+// // console.log(selectedFruits.length); //length
 
-// //OBJECT type (key, value)
-// let user = { //this obj
-//     firstname: 'F',//key:,value
-//     lastname: 'G',
-//     email: 'test@test.com',
-//     age: 26,
-//     location: 'bangkok',
-//     readlist:['html','css','javascript']
-// }
+// // //OBJECT type (key, value)
+// // let user = { //this obj
+// //     firstname: 'F',//key:,value
+// //     lastname: 'G',
+// //     email: 'test@test.com',
+// //     age: 26,
+// //     location: 'bangkok',
+// //     readlist:['html','css','javascript']
+// // }
 
-// console.log(user);
-// console.log(user.email);
-// user.age = 60;
-// console.log(user.age);
-// console.log(user.readlist.length-1); //length
+// // console.log(user);
+// // console.log(user.email);
+// // user.age = 60;
+// // console.log(user.age);
+// // console.log(user.readlist.length-1); //length
 
-//chaging data type
-let x = 123;
-x = 'F';
-console.log(x);
-console.log(typeof x); //type of
+// //chaging data type
+// // let x = 123;
+// x = 'F';
+// console.log(x);
+// console.log(typeof x); //type of
+
+// let number = twenty;
+// console.log(number);
+
+const number = [3,4];
+// console.log(number);
+// //end
+// number.push(5,6);
+// console.log(number);
+// //begining
+// number.unshift(1,2);
+// console.log(number);
+// //middle
+// number.splice(2,0,'a','b');
+// console.log(number);
+//removed at end
+// const last = number.pop();
+// number.pop();
+// console.log(number);
