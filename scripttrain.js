782d3f20be3e8fe3626ad6f9bfabf858b960913c
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-16 15:30:08 +0700

    factory object

--- scripttrain.js
+++ scripttrain.js
@@ -15,13 +15,38 @@
 // car.make = 'Folk';
 // car.model = 'E240';
 
-function Mycar (make, model) {
-    return{
-        make,model,drive(){
-            console.log('dirve');
-        }
+// function Mycar (make, model) {
+//     return{
+//         make,model,drive(){
+//             console.log('dirve');
+//         }
+//     }
+// };
+
+// const car1 = new mycar('Funk',240);
+// console.log(car1);
+
+// let Car = function(make,model,year) {
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+//     this.drive = function(name){
+//         console.log(`${name} is drive car`);
+//     }
+// };
+// let car1 = new Car('Flok','E250',2006);
+// console.log(car1);
+// console.log(car1.drive('Jackson'));
+
+let Pig = function(namep,yearp,location){
+    this.namep = namep;
+    this.yearp = yearp;
+    this.location = location;
+    this.Cut = function(nameB){
+        console.log(`${nameB} is Cut pig`);
     }
 };
 
-const car1 = new mycar('Funk',240);
-console.log(car1);
+let butcer = new Pig('COCO',5,'Brazill');
+console.log(butcer);
+butcer.Cut('Mozert');
